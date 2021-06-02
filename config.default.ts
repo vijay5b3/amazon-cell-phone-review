96903e9b5c0268a453483feadf282b67b707f9d7
vijay5b3 <68815512+vijay5b3@users.noreply.github.com>
2021-06-02 12:27:03 +0530

    Add files via upload

---
+++ config.default.ts
@@ -0,0 +1,29 @@
+export const brands = [
+  'ASUS',
+  'Apple',
+  'Google',
+  'HUAWEI',
+  'Motorola',
+  'Nokia',
+  'OnePlus',
+  'Samsung',
+  'Sony',
+  'Xiaomi',
+]
+
+export const brandKeywords = [
+  { brand: 'Apple', keywords: ['iPhone'] },
+  { brand: 'Google', keywords: ['Pixel'] },
+  { brand: 'HUAWEI', keywords: ['Honor'] },
+  { brand: 'Motorola', keywords: ['Moto'] },
+  { brand: 'Samsung', keywords: ['Haven'] },
+  { brand: 'Sony', keywords: ['Xperia'] },
+]
+
+export const categories = 'both'
+
+export const numberOfWorkers = 8
+
+export const useBrowserless = false
+export const browserlessEndpoint = 'wss://chrome.browserless.io'
+export const browserlessToken = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
